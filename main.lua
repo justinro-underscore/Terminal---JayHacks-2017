d84b2622b307abc58d1a2027e78eea95f3f85f43
Justin Roderman <jcroderman@gmail.com>
2017-02-17 21:25:21 -0600

    Adds a main

---
+++ main.lua
@@ -0,0 +1,21 @@
+function love.load()
+  love.window.setTitle("I wish that I had Jesse\'s Girl")
+
+	love.window.setFullscreen(true)
+
+
+end
+
+function love.update(dt)
+
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
+function love.draw()
+
+end
