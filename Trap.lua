7524ed060f4d86c179b16b748adb2b4ea8a6ff6a
harryl6798 <harryl6798@gmail.com>
2017-02-18 17:25:42 -0600

    Hey

---
+++ Trap.lua
@@ -0,0 +1,12 @@
+Trap ={}
+function Trap:new(x,y)
+  local o  ={}
+  setmetatable(o, {__index=self})
+  o.position = vector.new(x,y)
+  o.size = vector.new(96,32)
+  return o
+end
+
+function Trap:draw()
+  love.graphics.rectangle("fill", self.position.x, self.position.y, self.size.x, self.size.y)
+end
