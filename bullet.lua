5ea7d2694539858afc1490357c26be90ba00e572
harryl6798 <harryl6798@gmail.com>
2017-02-18 14:00:27 -0600

    adds bullet functionality

--- bullet.lua
+++ bullet.lua
@@ -1,10 +1,10 @@
 bullet = {}
-function bullet:new(x,y)
+function bullet:new(x,y,velocity_x,velocity_y)
   o= {}
   setMetaTable(o,{__index = self})
-
+  o.size(1,1)
   o.position = vector.new(x,y)
-  o.velocity = velocity.new(0,0)
+  o.velocity = velocity.new(velocity_x,velocity_y)
 
   o.VELOCITY_CONSTANT = 4000
 
@@ -12,4 +12,13 @@ function bullet:new(x,y)
 end
 
 function bullet:update(dt)
-  local
+  self.position = self.position + self.velocity.velocity_x *dt
+end
+
+function bullet:draw()
+  love.graphics.circle(circle, self.position.x, self.position.y)
+end
+
+function bullet:collision()
+
+end
